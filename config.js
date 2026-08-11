58e016a8d32449d6a550b763f521e2f793d2b6d8
geueyrgrrhstdgfehdjd-wq <geueyrgrrhstdgfehdjd@gmail.com>
2026-08-11 23:13:54 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // รหัส Token ของ Discord Bot
+  token: "MTUzNjczMzk4ODEzNDA2NDE1MQ.GIEz2s.pirBib6r_EODxFbpW__UQhp_4oB2j8jDLqTcyg",
+
+  // รายการ User ID ที่ได้รับอนุญาต (ใส่เป็น String)
+  allowedIds: [
+    "1504934379775070338"
+  ]
+};
